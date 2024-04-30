576a5c8ab88477bfd6bc9adcdb3dacd8b1ca5207
ZachGreene27 <159195573+ZachGreene27@users.noreply.github.com>
2024-04-30 12:32:41 -0400

    Update Ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -1,63 +1,77 @@
+import java.util.ArrayList;
+
 public class Ship {
-    private int [] xLocation;
-    private int [] yLocation;
-    private int numHits;
+        //ArrayList<Integer> Xshop = new ArrayList<>();
+       // ArrayList<Integer> Yshop = new ArrayList<>();
+        String name;
 
-    //public String checkLife(int[] row, int[] col)
-    //{
-        //if (row == this.xlocation);
-    //}
+        int xloc1, xloc2, yloc1, yloc2,n;
 
-    public void setLocations(String name, int[] xLocations, int[] yLocations)
-    {
-        this.xLocation = xLocations;
-        this.yLocation = yLocations;
-    }
 
-    if (n == 1)
-                {
-                        Xshop.add(x1);
-                        Yshop.add(y1);
-                }
-                else if (n == 2)
-                {
-                        Xshop.add(x1);
-                        Xshop.add(x2);
-                        Yshop.add(y1);
-                        Yshop.add(y2);
+        public Ship(int x1, int y1, int x2, int y2, String name, int n)
+        {
+                this.n = n;
+                this.xloc1 = x1;
+                this.xloc2 = x2;
+                this.yloc1 = y1;
+                this.yloc2 = y2;
+                this.name = name;
 
-                }
-                else
-                {
-                        if (x1 == x2)
-                        {
-                                for (int i = 0; i < n; i++)
-                                {
-                                        Xshop.add(x1);
-                                }
-                        }
-                        else
-                        {
-                                for (int i = x1; i <= x2; i++)
-                                {
-                                        Xshop.add(i);
-                                }
-                        }
+        }
+//
+//        public void setShip(int n, int x1, int y1, int x2, int y2, String name)
+//        {
+//                if (n == 1)
+//                {
+//                        Xshop.add(x1);
+//                        Yshop.add(y1);
+//                }
+//                else if (n == 2)
+//                {
+//                        Xshop.add(x1);
+//                        Xshop.add(x2);
+//                        Yshop.add(y1);
+//                        Yshop.add(y2);
+//
+//                }
+//                else
+//                {
+//                        if (x1 == x2)
+//                        {
+//                                for (int i = 0; i < n; i++)
+//                                {
+//                                        Xshop.add(x1);
+//                                }
+//                        }
+//                        else
+//                        {
+//                                for (int i = x1; i <= x2; i++)
+//                                {
+//                                        Xshop.add(i);
+//                                }
+//                        }
+//
+//                        if (y1 == y2)
+//                        {
+//                                for (int i = 0; i < n; i++)
+//                                {
+//                                        Yshop.add(y1);
+//                                }
+//                        }
+//                        else
+//                        {
+//                                for (int i = y1; i <= y2; i++)
+//                                {
+//                                        Yshop.add(i);
+//                                }
+//                        }
+//                }
+//        }
 
-                        if (y1 == y2)
-                        {
-                                for (int i = 0; i < n; i++)
-                                {
-                                        Yshop.add(y1);
-                                }
-                        }
-                        else
-                        {
-                                for (int i = y1; i <= y2; i++)
-                                {
-                                        Yshop.add(i);
-                                }
-                        }
-                }
-
+        public String getLocation()
+        {
+                String firstBlock = "Your ship has a starting block at the location of " + xloc1 + ", " + yloc1;
+                String endBlock = "and your ship has an ending at " + xloc2 + ", " + yloc2;
+                return firstBlock + endBlock;
+        }
 }
