6fc951ee4da12e47a56c81d4c5b65da9aaca00ac
ZachGreene27 <159195573+ZachGreene27@users.noreply.github.com>
2024-04-29 20:48:17 -0400

    Update Ship.java

--- src/Ship.java
+++ src/Ship.java
@@ -14,4 +14,50 @@ public class Ship {
         this.yLocation = yLocations;
     }
 
+    if (n == 1)
+                {
+                        Xshop.add(x1);
+                        Yshop.add(y1);
+                }
+                else if (n == 2)
+                {
+                        Xshop.add(x1);
+                        Xshop.add(x2);
+                        Yshop.add(y1);
+                        Yshop.add(y2);
+
+                }
+                else
+                {
+                        if (x1 == x2)
+                        {
+                                for (int i = 0; i < n; i++)
+                                {
+                                        Xshop.add(x1);
+                                }
+                        }
+                        else
+                        {
+                                for (int i = x1; i <= x2; i++)
+                                {
+                                        Xshop.add(i);
+                                }
+                        }
+
+                        if (y1 == y2)
+                        {
+                                for (int i = 0; i < n; i++)
+                                {
+                                        Yshop.add(y1);
+                                }
+                        }
+                        else
+                        {
+                                for (int i = y1; i <= y2; i++)
+                                {
+                                        Yshop.add(i);
+                                }
+                        }
+                }
+
 }
